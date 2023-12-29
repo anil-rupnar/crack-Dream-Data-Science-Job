9b84b07630ecb40f7f8cd8434fac3e3dcdfb54ff
Anil Rupnar <101308099+anil-rupnar@users.noreply.github.com>
2023-12-29 11:36:44 +0530

    first commit

---
+++ 120Dayschallenge/demo.py
@@ -0,0 +1,45 @@
+"""
+120 days crack Data science Job challenge 🎯.
+Excited to tackle Day 4 of the 120-day crack Data science Job challenge!
+
+#Day4
+
+Todyas learning:
+1. For loop
+
+Language:
+Python[ipykernal]
+
+Software:
+Vscode
+
+Python mentor:
+(sudhanshu kumar sir)
+
+
+Practice Code :
+https://github.com/anil-rupnar/crack-Dream-Data-Science-Job/blob/main/120Dayschallenge/Day3/dict%20%2C%20conditional%20%2Cloop%20.ipynb
+
+Assingment:
+
+Assignment solution :
+https://github.com/anil-rupnar/crack-Dream-Data-Science-Job/blob/main/120Dayschallenge/Day3/Notes.ipynb
+
+Quiz :
+https://www.geeksforgeeks.org/python-dictionary/
+https://www.w3schools.com/python/python_dictionaries.asp
+
+Github commands:
+https://github.com/anil-rupnar/crack-Dream-Data-Science-Job/blob/main/git%20commads.txt
+
+
+Keep coding, stay motivated, and remember: that every challenge is an opportunity to grow! 🌱
+Share your goals in the comments below! 👇 Let's inspire each other! 🔥
+
+sudhanshu kumar
+
+#CodingChallenge 
+#CodeOptimization 
+#DailyCoding 
+
+"""
---
+++ Readme.md
@@ -0,0 +1,22 @@
+# Crack Dream Science Job challenge🎯
+
+
+This is my Crack Dream Science Job challenge🎯, step by step, rooted in achieving your dream and mine as well. I hope you will join me in this challenge. It spans 120 days, presenting a tough but rewarding journey for me. Nevertheless, I am determined to overcome it.
+
+---
+
+🔍 **Key Highlights:**
+- Day-by-day progress towards the dream job.
+- Unveiling the challenges faced and lessons learned.
+- Insights into the science behind cracking the dream job code.
+
+
+Let's build a community of support and motivation. 💬 #CareerJourney #DreamJobQuest
+
+
+🔗 Explore more:
+  - https://www.linkedin.com/in/anil-rupnar/
+---
+
+#DreamJobChallenge #CareerGoals #ProfessionalJourney #CareerDevelopment #SuccessMindset #DreamJobQuest #JobSearchTips #CareerInspiration #ScienceJobs #CareerProgress
+
